81558a34864ce550d0ab7221193eaeeb3b8ea4bc
Elisariane <elisarianeBS@gmail.com>
2019-03-27 21:12:27 -0300

    novas implementações

--- Sistemas operacionais /py-arch-os/pyos.py
+++ Sistemas operacionais /py-arch-os/pyos.py
@@ -13,8 +13,9 @@ class os_t:
 		self.terminal.enable_curses()
 
 		self.console_str = ""
-		self.terminal.console_print("this is the console, type the commands here\n 'exit' if you want stop ")
-
+		self.command_line = ""
+		self.terminal.console_print("this is the console, type the commands here\n 'exit' if you want stop\n>")
+	
 	def printk(self, msg):
 		self.terminal.kernel_print("kernel: " + msg + "\n")
 
@@ -26,35 +27,36 @@ class os_t:
 
 	def interrupt_keyboard (self):
 		key = self.terminal.get_key_buffer()
-
+		
 		if ((key >= ord('a')) and (key <= ord('z'))) or ((key >= ord('A')) and (key <= ord('Z'))) or ((key >= ord('0')) and (key <= ord('9'))) or (key == ord(' ')) or (key == ord('-')) or (key == ord('_')) or (key == ord('.')):
 			strchar = chr(key)
+			self.console_str += strchar 
 			self.terminal.console_print(strchar)
-
-
-		elif key == curses.KEY_BACKSPACE:
-			self.delete()
+			return
+		if key == curses.KEY_BACKSPACE:
+			self.console_str = self.console_str[0: len(self.console_str) - 1  ]
+			self.terminal.console_print("\r" + self.console_str)
             #self.reset_top()
 			
 			return
-		elif (key == curses.KEY_ENTER) or (key == ord('\n')):
+		if (key == curses.KEY_ENTER) or (key == ord('\n')):
 			
+			self.console_comandos()
+			self.terminal.console_print("\r")
 			return
 	
-	#
+		
 	def handle_interrupt (self, interrupt):
 		if (interrupt == pycfg.INTERRUPT_KEYBOARD):
 			self.interrupt_keyboard ()
-			self.terminal.console_print("\ninterrompendo o teclado!\n")
-		elif (interrupt == pycfg.INTERRUPT_TIMER):
-			self.interrupt_timer ()
-			self.terminal.console_print("\nIninterrompendo o ciclo\n")
-		elif (interrupt == pycfg.INTERRUPT_MEMORY_PROTECTION_FAULT):
-			self.interrupt_memory_protection_fault ()
-			self.terminal.console_print("\nIninterrompendo memoria\n")
+		#elif (interrupt == pycfg.INTERRUPT_TIMER):
+			#self.interrupt_timer ()
+			#self.terminal.console_print("\nIninterrompendo o ciclo\n")
+		#elif (interrupt == pycfg.INTERRUPT_MEMORY_PROTECTION_FAULT):
+			#self.interrupt_memory_protection_fault ()
+			#self.terminal.console_print("\nIninterrompendo memoria\n")
 			
-			
-		#self.terminal.console_print("teste")	
+
 		return
 
 	def syscall (self):
@@ -62,15 +64,12 @@ class os_t:
 		#self.terminal.app_print(msg)
 		return
 		
-	def console_comandos(self):
-		key = self.terminal.get_key_buffer()
-		self.console_str = key
-		strchar = chr(console_str)
-		if( str or chr == "exit"):	
-
+	def console_comandos(self):		
+		
+		if( self.console_str == "exit"):	
 			self.terminal.console_print("Bye Bye ...")
 			self.terminal.end()
-			
+			self.cpu.cpu_alive = False
 			
 
 				
